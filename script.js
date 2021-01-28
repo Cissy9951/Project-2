9f11e975e60e6b1f2b288d1da2aa7463772033b6
Cissy9951 <35778450+Cissy9951@users.noreply.github.com>
2021-01-27 19:34:59 -0500

    file link

--- script.js
+++ script.js
@@ -5,30 +5,30 @@ $(document).ready(function() {
         $("body").css("transition","background-image 1s");
         $("body").css("transition-timing-function","ease-in-out");
 
-        $("body").css("background-image", "url(/images/IMG_1259.jpg");
+        $("body").css("background-image", "url(../images/IMG_1259.jpg");
     } 
     function pic2(){
         $("body").css("transition","background-image 1s");
         $("body").css("transition-timing-function","ease-in-out");
 
-        $("body").css("background-image", "url(/images/Blue_IMG_0863.jpg");
+        $("body").css("background-image", "url(../images/Blue_IMG_0863.jpg");
     }
     function pic3(){
         $("body").css("transition","background-image 1s");
         $("body").css("transition-timing-function","ease-in-out");
 
-        $("body").css("background-image", "url(/images/Sensuous_IMG_1111.jpg");
+        $("body").css("background-image", "url(../images/Sensuous_IMG_1111.jpg");
     }
     function pic4(){
         $("body").css("transition","background-image 1s");
         $("body").css("transition-timing-function","ease-in-out");
 
-        $("body").css("background-image", "url(/images/IMG_1436.jpg");
+        $("body").css("background-image", "url(../images/IMG_1436.jpg");
     }
     function pic5(){
         $("body").css("transition","background-image 1s");
         $("body").css("transition-timing-function","ease-in-out");
-        $("body").css("background-image", "url(/images/IMG_0773.jpg");
+        $("body").css("background-image", "url(../images/IMG_0773.jpg");
     }
 
     let array = [pic1,pic2,pic3,pic4,pic5];
